003e25be99d29dd0c2348945c1543d26bd4e99bd
Jeremy Osterhoudt <jeremy.osterhoudt@ge.com>
2025-07-24 12:42:51 -0700

    Add unit tests and fix lint errors

---
+++ core/src/test/java/org/apache/calcite/avatica/SqlTypeTest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.calcite.avatica;
+
+import org.junit.Test;
+
+import java.sql.Types;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test class for {@link SqlType}.
+ */
+public class SqlTypeTest {
+
+  @Test public void ensureAnyIsGivenForNonStandardSqlTypes() {
+    // Arrange
+    // Oracle TIMESTAMP WITH TIME ZONE
+    int timestampWithTimeZoneTypeValue = -101;
+    // SQL server DateTimeOffset
+    int dateTimeOffset = -155;
+
+    // Act
+    SqlType tsSqlType = SqlType.valueOf(timestampWithTimeZoneTypeValue);
+    SqlType dtoSqlType = SqlType.valueOf(dateTimeOffset);
+
+    // Assert
+    assertEquals(SqlType.ANY, tsSqlType);
+    assertEquals(SqlType.ANY, dtoSqlType);
+  }
+  
+  @Test public void ensureStandardSqlTypesAreFound() {
+    // Arrange
+    int doubleTypeValue = Types.DOUBLE;
+    int varcharTypeValue = Types.VARCHAR;
+
+    // Act
+    SqlType doubleSqlType = SqlType.valueOf(doubleTypeValue);
+    SqlType varcharSqlType = SqlType.valueOf(varcharTypeValue);
+
+    // Assert
+    assertEquals(SqlType.DOUBLE, doubleSqlType);
+    assertEquals(SqlType.VARCHAR, varcharSqlType);
+  }
+}
